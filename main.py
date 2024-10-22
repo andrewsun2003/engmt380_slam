786d2ed90a1b74f97781386a7ae208be22f76cd2
andrewsun2003 <andrewsun2003@gmail.com>
2024-10-22 23:00:37 +1300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import slamBotHD as sb
 import numpy as np      
 import cv2 as cv 
 import math        
-import time
 import csv
 import next_locations as nl
 import movement as mv
 import bot_math as bm
+import compare_maps as cm
 
 sb.startUp()  
 sb.readCoreData()
@@ -59,7 +59,7 @@ def draw_boundary(current_angle, bot_x, bot_y, imgRow):
     current_angle = np.radians(current_angle)
 
     for dist in imgRow:
-        if dist != 0 and dist < 2:
+        if dist != 0 and dist < 2.5:
             dist2 = (dist / math.cos(stAngle)) * 100
             x = bot_x + dist2 * math.cos(stAngle - current_angle)
             y = bot_y + dist2 * math.sin(stAngle - current_angle)
@@ -83,6 +83,11 @@ def update_bot(bot_x, bot_y, current_angle):
 def full_rotation(initial_angle, bot_x, bot_y):
     step_angle = 30
     current_angle = sb.imuYaw
+    if (current_angle < step_angle) and current_angle >= 0:
+        while int(current_angle) != (int(step_angle)+10):
+            current_angle = sb.imuYaw
+            mv.rotate_left()
+    initial_angle = sb.imuYaw
     
     while int(current_angle) == int(initial_angle):
         current_angle = sb.imuYaw
@@ -92,12 +97,13 @@ def full_rotation(initial_angle, bot_x, bot_y):
         mv.rotate_left()
         current_angle = sb.imuYaw
         print(current_angle)
+        print("Initial Angle", int(initial_angle))
         update_bot(bot_x, bot_y, current_angle)
         cv.waitKey(1)
 
         if int(current_angle) % int(step_angle) == 0:
             mv.stop_moving()
-            cv.waitKey(30)
+            cv.waitKey(100)
             current_angle = sb.imuYaw
             imgRow = read_img()
             draw_boundary(current_angle, bot_x, bot_y, imgRow)
@@ -105,16 +111,21 @@ def full_rotation(initial_angle, bot_x, bot_y):
             while int(current_angle) % int(step_angle) == 0:
                 mv.rotate_left()
                 current_angle = sb.imuYaw
+
+    return current_angle
             
         
 # Initialize maps
 map = np.ones([800, 800], np.uint8) * 255
 map = cv.cvtColor(map, cv.COLOR_GRAY2BGR)
+previous_map = map
 bot_x, bot_y = 400, 400
 key = 0
+iterations = 0
 
 print(f'Battery: {sb.botBatt}V')
 
+#-----------------------------Main-------------------------------#
 
 while True:
     
@@ -122,9 +133,26 @@ while True:
 
     initial_angle = sb.imuYaw
 
-    full_rotation(initial_angle, bot_x, bot_y)
+    current_angle = full_rotation(initial_angle, bot_x, bot_y)
+    cv.waitKey(0)
+    combined_map, current_corners, previous_corners = cm.main(map, previous_map)
+    
+    kernel = np.ones((3, 3), np.uint8)
+    combined_map = cv.erode(combined_map, kernel, iterations=5)
 
-    path = nl.main(map, (bot_x, bot_y))
+    cv.imshow("combined map", combined_map)
+    cv.imshow("current corners", current_corners)
+    cv.imshow("previous corners", previous_corners)
+
+    cv.waitKey(10)
+
+    if iterations == 2:
+        
+        while key != 27:
+            key = cv.waitKey(0)
+        break
+
+    path = nl.main(combined_map, (bot_x, bot_y))
     print("Path", path)
 
     for i in range(len(path) - 1):
@@ -135,23 +163,27 @@ while True:
         new_heading = bm.heading(current_point, next_point, current_angle)
         print("New Heading", int(new_heading))
         # Adjust bot's heading
-        
-        if bm.angle_difference(int(new_heading), int(np.degrees(current_angle))) < 0:
-            while int(new_heading) != int(np.degrees(current_angle)):
+        if abs(new_heading) == 180:
+            new_heading = 179
+
+        if bm.angle_difference(int(new_heading), int(current_angle)) > 0:
+            while int(current_angle) != int(new_heading):
                 mv.rotate_left()
-                current_angle = np.radians(sb.imuYaw)
-                print("Current Angle", np.degrees(current_angle))
+                print("Current Angle", current_angle)
                 print("New Heading", int(new_heading))
+                current_angle = sb.imuYaw
 
         else:
-            while int(new_heading) != int(np.degrees(current_angle)):
+            while int(current_angle) != int(new_heading):
                 mv.rotate_right()
-                print("Current Angle", np.degrees(current_angle))
+                print("Current Angle", current_angle)
                 print("New Heading", int(new_heading))
-                current_angle = np.radians(sb.imuYaw)
+                current_angle = sb.imuYaw
+                
         print("moving forward")
         
         mv.stop_moving()
+        cv.waitKey(10)
 
         # Move towards the next point if heading is correct
         mv.move_to_next_position(current_point, next_point, current_angle)
@@ -160,8 +192,17 @@ while True:
         update_bot(bot_x, bot_y, current_angle)
     
         start_angle = current_angle
+
+    # Reset map and set previous map to the combined map
+    previous_map = combined_map
     
-    key = cv.waitKey(0) & 0xFF
+    cv.imshow("Previous Map", previous_map)
+    map = np.ones([800, 800], np.uint8) * 255
+    map = cv.cvtColor(map, cv.COLOR_GRAY2BGR)
+
+    iterations += 1
+
+    key = cv.waitKey(1) & 0xFF
     if key == ord('q'):
         break
 
