2f138c551f6e744b95c00dbf589f381b759e7649
andrewsun2003 <andrewsun2003@gmail.com>
2024-10-24 22:06:25 +1300

    Update bot_math.py

--- bot_math.py
+++ bot_math.py
@@ -1,40 +1,36 @@
 import numpy as np
 import math
+import cv2 as cv
 
 def distance(pt1, pt2):
     return np.linalg.norm(np.array(pt1) - np.array(pt2))
 
 
 def angle_difference(angle1, angle2):
-	if (angle1 > 0 and angle2 > 0):
+	if (angle1 >= 0 and angle2 >= 0):
 		difference = angle1 - angle2
 	if (angle1 < 0 and angle2 < 0):
 		difference = angle1 - angle2
 
-	if (angle1 > 0 and angle2 < 0):
-		difference = -360*(angle1 - angle2)
+	if (angle1 >= 0 and angle2 < 0):
+		difference = -360+(angle1 - angle2)
 		
-	if (angle1 < 0 and angle2 > 0):
-		difference = 360*(angle1 - angle2)
+	if (angle1 < 0 and angle2 >= 0):
+		difference = 360+(angle1 - angle2)
 		
 	return difference
 
-def heading(current_point, next_point, current_angle):
+def heading(current_point, next_point):
     # Calculate the difference in coordinates
-    delta_x = angle_difference(next_point[0], current_point[0])
-    delta_y = angle_difference(next_point[1], current_point[1])
-    
-    # Calculate the angle to the next point in radians
-    angle_to_next = math.atan2(delta_y, delta_x)  # Angle in radians
-
-    # Convert angle to degrees
-    angle_to_next_degrees = math.degrees(angle_to_next)
-
-    # Calculate the heading by adjusting for the current angle
-    heading_angle = -(angle_to_next_degrees - current_angle)
-
-    # Normalize the heading angle to be between -180 and 180 degrees
-    heading_angle = (heading_angle + 180) % 360 - 180
-    
+    delta_x = next_point[0] - current_point[0]
+    delta_y = (800 - next_point[1]) - (800 - current_point[1])
+	
+    print("delta y ", delta_y)
+    print("delta x", delta_x)
+	
+    heading_angle = math.degrees(math.atan2(delta_y, delta_x))  # Angle in radians
+ 
+    print("heading", heading_angle)
+	
     return heading_angle
 
