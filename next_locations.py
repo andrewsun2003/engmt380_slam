efc0a9b5bbcfafdadaadb6c90e182da4a6b405b1
andrewsun2003 <andrewsun2003@gmail.com>
2024-10-24 22:08:17 +1300

    Update next_locations.py

--- next_locations.py
+++ next_locations.py
@@ -3,6 +3,12 @@ import numpy as np
 import astar
 import bot_math as bm
 
+# Global lists for corners and contour ends
+contour_ends = []
+contour_end_groups = []
+open_space_points = []
+pairs = []
+
 
 def process_image(map):
     map = cv.cvtColor(map, cv.COLOR_BGR2GRAY)
@@ -17,11 +23,8 @@ def process_image(map):
                 cv.circle(dilated_map, (x, y), radius=5, color=255, thickness=-1)
 
     _, binary_map = cv.threshold(dilated_map, 127, 255, cv.THRESH_BINARY)
-    cv.imshow("binary map", binary_map)
-    cv.waitKey(0)
     edges = cv.Canny(binary_map, 127, 255, apertureSize=3)
-    cv.imshow("edges", edges)
-    cv.waitKey(0)
+
     approx_map = np.zeros(map.shape, dtype='uint8')
 
     contours, hierarchy = cv.findContours(edges, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
@@ -30,23 +33,13 @@ def process_image(map):
     filtered_contours = []
 
     for contour in contours:
-        area = cv.contourArea(contour)
-        if area > 500:
-            perimeter = cv.arcLength(contour, True)
-            epsilon = 0.01 * perimeter  
+        perimeter = cv.arcLength(contour, True)
+        if perimeter > 200:
+            epsilon = 0.023 * perimeter  
             approx = cv.approxPolyDP(contour, epsilon, True)
             approx_points.append(approx)
             cv.drawContours(approx_map, [contour], -1, 255, 1)
             filtered_contours.append(contour)
-    contour_map = np.zeros(map.shape, dtype='uint8')
-    i = 50
-    for contour in contours:
-        cv.drawContours(contour_map, [contour], -1, i, 1)
-        i+=100
-    cv.imshow("contours", contour_map)
-    cv.waitKey(0)
-    cv.imshow("approx", approx_map)
-    cv.waitKey(0)
     return approx_map, approx_points, contours, filtered_contours
 
 
@@ -60,33 +53,32 @@ def calculate_angle(pt1, pt2, pt3):
     dot_product = np.dot(vec1, vec2)
     magnitude1 = np.linalg.norm(vec1)
     magnitude2 = np.linalg.norm(vec2)
-    
+
     # Calculate the angle in radians and convert it to degrees
     cos_angle = dot_product / (magnitude1 * magnitude2)
     angle = np.arccos(np.clip(cos_angle, -1.0, 1.0))  # Clip the value to avoid floating-point errors
     return np.degrees(angle)
-
 
 def average_points(group):
     avg_x = int(np.mean([p[0] for p in group]))
     avg_y = int(np.mean([p[1] for p in group]))
     return (avg_x, avg_y)
 
-
 def find_ends(approx_points):
+    global contour_ends, contour_end_groups
     for point in approx_points:
         for i in range(len(point)):
             pt1 = point[i-1][0]
             pt2 = point[i][0]
-            pt3 = point[(i+1)%len(point)][0]
+            pt3 = point[(i+1) % len(point)][0]
             
             angle = calculate_angle(pt1, pt2, pt3)
             
             # Check if the angle is approximately 90 degrees (within a tolerance)
-            if (angle < 80 or angle > 100):
+            if angle < 60 or angle > 120:
                 contour_ends.append(pt2)   
 
-    threshold_distance = 100
+    threshold_distance = 80
     for contour_end in contour_ends:
         added = False
         for group in contour_end_groups:
@@ -97,7 +89,6 @@ def find_ends(approx_points):
                 break
         if not added:
             contour_end_groups.append([contour_end])  # Create a new group if no close group was found
-
 
 def find_closest_pairs(points):
     pairs = []
@@ -113,69 +104,96 @@ def find_closest_pairs(points):
         pairs.append((point1, closest_point))
     return pairs
 
-
-def find_open_space_points():
+def find_open_space_points(pairs, contours):
+    global open_space_points
     for i in range(len(pairs)):
-        x = int((pairs[i][0][0] + pairs[i][1][0])/2)
-        y = int((pairs[i][0][1] + pairs[i][1][1])/2)
-        open_space_points.append((x,y))
+        x = int((pairs[i][0][0] + pairs[i][1][0]) / 2)
+        y = int((pairs[i][0][1] + pairs[i][1][1]) / 2)
+        for contour in contours:
+            isInside = cv.pointPolygonTest(contour, (x,y), True)
+            if isInside >= -25:
+                isInside = True
+                break
+            else:
+                isInside = False
+        if isInside == False:
+            open_space_points.append((x, y))
 
-
-def find_next_position():
+def find_next_position(current_position):
+    global open_space_points
     max_index = 0
     max_distance = 0
-    print(open_space_points)
 
     for i, point in enumerate(open_space_points):
-        d = bm.distance(current_position, point)
-        if (d > max_distance):
+        d = bm.distance((400,400), point)
+        if d > max_distance:
             max_distance = d
             max_index = i
+
     next_position = open_space_points[max_index]
 
     return next_position
 
+def show_path(path, approx_map):
+    for point in path:
+        cv.circle(approx_map, point, 1, [255, 255, 0], 2)
 
-map  = cv.imread('example1.png')
-approx_map, approx_points, contours, filtered_contours = process_image(map)
-contours = filtered_contours
+    cv.imshow("Path", approx_map)
 
-current_position = (400, 400)
+def main(map, current_position):
+    if map is None:
+        print("Error: Could not read the image.")
+        return
 
-contour_ends = []
-contour_end_groups = []
-open_space_points = []
-pairs = []
-find_ends(approx_points)
+    approx_map, approx_points, contours, filtered_contours= process_image(map)
+    contours = filtered_contours
 
-averaged_ends = [average_points(contour_end_group) for contour_end_group in contour_end_groups]
+    cv.imshow("approx", approx_map)
+    cv.waitKey(0)
+    
+    find_ends(approx_points)
 
-pairs = find_closest_pairs(averaged_ends)
-find_open_space_points()
+    averaged_ends = [average_points(contour_end_group) for contour_end_group in contour_end_groups]
 
-next_position = find_next_position()
+    pairs = find_closest_pairs(averaged_ends)
+    if pairs is None:
+        print("No pairs")
+        return
+    
+    find_open_space_points(pairs, contours)
 
-for averaged_end in averaged_ends:
-    cv.circle(approx_map, tuple(averaged_end), 5, 150, -1)
+    if open_space_points is None:
+        print("No opensapce points")
+        return
 
-approx_map = cv.cvtColor(approx_map, cv.COLOR_GRAY2BGR)
-cv.circle(approx_map, (current_position), 15, [0, 255, 0], -1)
-cv.circle(approx_map, (next_position), 15, [0, 0, 255], -1)
+    next_position = find_next_position(current_position)
+        
+    approx_map = cv.cvtColor(approx_map, cv.COLOR_GRAY2BGR)
 
-path = astar.a_star_pathfinding(current_position, next_position, (contours), approx_map)
+    for averaged_end in averaged_ends:
+        cv.circle(approx_map, tuple(averaged_end), 5, [0, 255, 255], -1)
 
-if path == None:
-    print("No path found")
-else:
-    path = path[::25]
-    path.append(next_position)
-    print(f'Path: {path}')
-    for i in range(len(path)):
-        cv.circle(approx_map, path[i], 1, [255, 255, 0], 2)
+    for point in open_space_points:
+        cv.circle(approx_map, tuple(point), 5, [255, 0, 0], -1)
+    
+    cv.circle(approx_map, tuple(current_position), 15, [0, 255, 0], -1)
+    cv.circle(approx_map, tuple(next_position), 15, [0, 0, 255], -1)
 
+    path = astar.a_star_pathfinding(current_position, next_position, contours, approx_map)
 
-while(True):
-    cv.imshow("Processed Map", approx_map)
-    key = cv.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
+    if path is not None:
+        path = path[::25]
+        path.append(next_position)
+    else:
+        print("No Path")
+        return
+
+    cv.imshow("A_star", approx_map)
+    print("Path", path)
+
+    show_path(path, approx_map)
+
+    cv.waitKey(50)
+
+    return path
+
